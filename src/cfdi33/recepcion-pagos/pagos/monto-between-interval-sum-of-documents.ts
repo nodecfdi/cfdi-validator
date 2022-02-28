08b6e529f67f7ebc3152673b08da21ef995fed55
Fernando Isidro L <luffynando@gmail.com>
2022-02-28 04:01:20 -0600

    feat: migrate from php to node MontoBetweenIntervalSumOfDocuments

---
+++ src/cfdi33/recepcion-pagos/pagos/monto-between-interval-sum-of-documents.ts
@@ -0,0 +1,94 @@
+import { AbstractPagoValidator } from './abstract-pago-validator';
+import { use } from 'typescript-mix';
+import { CalculateDocumentAmountTrait } from '../helpers/calculate-document-amount-trait';
+import { CNodeInterface, CurrencyDecimals } from '@nodecfdi/cfdiutils-common';
+import { ValidatePagoException } from './validate-pago-exception';
+
+interface MontoBetweenIntervalSumOfDocuments extends AbstractPagoValidator, CalculateDocumentAmountTrait {}
+
+/**
+ * PAGO09: En un pago, el monto del pago debe encontrarse entre límites mínimo y máximo de la suma
+ *         de los valores registrados en el importe pagado de los documentos relacionados (Guía llenado)
+ */
+class MontoBetweenIntervalSumOfDocuments extends AbstractPagoValidator {
+    @use(CalculateDocumentAmountTrait) private this: unknown;
+
+    protected code = 'PAGO09';
+
+    protected title = [
+        'En un pago, el monto del pago debe encontrarse entre límites mínimo y máximo de la suma',
+        ' de los valores registrados en el importe pagado de los documentos relacionados (Guía llenado)',
+    ].join('');
+
+    public validatePago(pago: CNodeInterface): boolean {
+        const pagoAmount = parseFloat(pago.attributes().get('Monto') || '0');
+        const bounds = this.calculateDocumentsAmountBounds(pago);
+        const currencyDecimals = CurrencyDecimals.newFromKnownCurrencies(pago.attributes().get('MonedaP') || '', 2);
+        const lower = currencyDecimals.round(bounds['lower']);
+        const upper = currencyDecimals.round(bounds['upper']);
+        if (pagoAmount < lower || pagoAmount > upper) {
+            throw new ValidatePagoException(
+                `Monto del pago: "${pagoAmount}", Suma mínima: "${lower}", Suma máxima: "${upper}"`
+            );
+        }
+
+        return true;
+    }
+
+    public calculateDocumentsAmountBounds(pago: CNodeInterface): Record<string, number> {
+        const documents = pago.searchNodes('pago10:DoctoRelacionado');
+        const values = documents.map((document) => this.calculateDocumentAmountBounds(document, pago));
+        return {
+            lower: values.reduce((a, b) => a + (b['lower'] || 0), 0),
+            upper: values.reduce((a, b) => a + (b['upper'] || 0), 0),
+        };
+    }
+
+    public calculateDocumentAmountBounds(
+        doctoRelacionado: CNodeInterface,
+        pago: CNodeInterface
+    ): Record<string, number> {
+        const amount = this.calculateDocumentAmount(doctoRelacionado, pago);
+        const impPagado = doctoRelacionado.attributes().get('ImpPagado') ?? amount;
+        const tipoCambioDR = doctoRelacionado.attributes().get('TipoCambioDR');
+        let exchangeRate = 1;
+        if ('' !== tipoCambioDR && pago.attributes().get('MonedaP') !== pago.attributes().get('MonedaDR')) {
+            exchangeRate = parseFloat(tipoCambioDR || '0');
+        }
+        const numDecimalsAmount = this.getNumDecimals(`${impPagado}`);
+        const numDecimalsExchangeRate = this.getNumDecimals(`${tipoCambioDR}`);
+
+        if (0 === numDecimalsExchangeRate) {
+            return {
+                lower: amount / exchangeRate,
+                upper: amount / exchangeRate,
+            };
+        }
+
+        const almostTwo = 2 - 10 ** -10;
+
+        const lowerAmount = amount - 10 ** -numDecimalsAmount / 2;
+        const lowerExchangeRate = exchangeRate + 10 ** -numDecimalsExchangeRate / almostTwo;
+
+        const upperAmount = amount + 10 ** -numDecimalsAmount / almostTwo;
+        const upperExchangeRate = exchangeRate - 10 ** -numDecimalsExchangeRate / 2;
+        return {
+            lower: lowerAmount / lowerExchangeRate,
+            upper: upperAmount / upperExchangeRate,
+        };
+    }
+
+    public getNumDecimals(numeric: string): number {
+        if (isNaN(Number(numeric)) || isNaN(parseFloat(numeric))) {
+            return 0;
+        }
+        const pointPosition = numeric.indexOf('.');
+        if (pointPosition === -1) {
+            return 0;
+        }
+
+        return numeric.length - 1 - pointPosition;
+    }
+}
+
+export { MontoBetweenIntervalSumOfDocuments };
---
+++ tests/unit/cfdi33/recepcion-pagos/pagos/monto-between-interval-sum-of-documents.test.ts
@@ -0,0 +1,105 @@
+import { Pagos10 } from '@nodecfdi/cfdiutils-elements';
+import { MontoBetweenIntervalSumOfDocuments } from '../../../../../src/cfdi33/recepcion-pagos/pagos/monto-between-interval-sum-of-documents';
+import { ValidatePagoException } from '../../../../../src/cfdi33/recepcion-pagos/pagos/validate-pago-exception';
+
+describe('MontoBetweenIntervalSumOfDocuments', () => {
+    const { Pago } = Pagos10;
+    const providerValidWithRandomAmounts = (): number[][] => {
+        const randomValues: number[][] = [];
+        for (let i = 0; i < 20; i++) {
+            randomValues.push([(Math.random() * (1 - 99999999) + 99999999) / 100]);
+        }
+        return randomValues;
+    };
+
+    test('valid', () => {
+        const pago = new Pago({
+            MonedaP: 'USD',
+            Monto: '123.45',
+        });
+        pago.multiDoctoRelacionado(
+            ...[
+                { ImpPagado: '50.00' }, // 50.00
+                { MonedaDR: 'EUR', TipoCambioDR: '0.50', ImpPagado: '25.00' }, // 25.00 / 0.50 => 50
+                { MonedaDR: 'MXN', TipoCambioDR: '18.7894', ImpPagado: '440.61' }, // 440.61 / 18.7894 => 23.45
+            ]
+        );
+        const validator = new MontoBetweenIntervalSumOfDocuments();
+
+        expect(validator.validatePago(pago)).toBeTruthy();
+    });
+
+    test.each([['122.93'], ['123.98']])('invalids', (monto) => {
+        const pago = new Pago({
+            MonedaP: 'USD',
+            Monto: monto,
+        });
+        pago.multiDoctoRelacionado(
+            ...[
+                { ImpPagado: '20.00' }, // 20.00
+                { MonedaDR: 'USD', ImpPagado: '30.00' }, // 30.00
+                { MonedaDR: 'EUR', TipoCambioDR: '0.50', ImpPagado: '25.00' }, // 25.00 / 0.50 => 50
+                { MonedaDR: 'MXN', TipoCambioDR: '18.7894', ImpPagado: '440.61' }, // 440.61 / 18.7894 => 23.45
+            ]
+        );
+        const validator = new MontoBetweenIntervalSumOfDocuments();
+
+        expect.hasAssertions();
+        try {
+            validator.validatePago(pago);
+        } catch (e) {
+            expect(e).toBeInstanceOf(ValidatePagoException);
+        }
+    });
+
+    test('valid with several decimals', () => {
+        // payment was made of 5137.42 USD (ER: 18.7694) => 96426.29 MXN
+        // to pay a document on USD
+        const pago = new Pago({
+            MonedaP: 'MXN',
+            Monto: '96426.29',
+        });
+        pago.addDoctoRelacionado({
+            MonedaDR: 'USD',
+            TipoCambioDR: Number(1 / 18.7694).toFixed(4),
+            ImpPagado: '5137.42',
+        });
+        const validator = new MontoBetweenIntervalSumOfDocuments();
+
+        expect(validator.validatePago(pago)).toBeTruthy();
+    });
+
+    test('valid with multi documents', () => {
+        const pago = new Pago({
+            MonedaP: 'MXN',
+            Monto: '21588.07',
+        });
+        pago.multiDoctoRelacionado(
+            ...[
+                { MonedaDR: 'MXN', ImpPagado: '6826.60' },
+                { MonedaDR: 'MXN', ImpPagado: '2114.52' },
+                { MonedaDR: 'MXN', ImpPagado: '11245.04' },
+                { MonedaDR: 'MXN', ImpPagado: '1401.91' },
+            ]
+        );
+        const validator = new MontoBetweenIntervalSumOfDocuments();
+
+        expect(validator.validatePago(pago)).toBeTruthy();
+    });
+
+    test.each([...providerValidWithRandomAmounts()])('valid with random amounts', (amount) => {
+        const pago = new Pago({
+            MonedaP: 'MXN',
+            Monto: amount.toFixed(2),
+        });
+        pago.multiDoctoRelacionado(
+            ...[
+                { MonedaDR: 'MXN', ImpPagado: (amount / 3).toFixed(2) },
+                { MonedaDR: 'MXN', ImpPagado: ((2 * amount) / 3).toFixed(2) },
+            ]
+        );
+        const validator = new MontoBetweenIntervalSumOfDocuments();
+
+        expect(validator.validatePago(pago)).toBeTruthy();
+    });
+});
