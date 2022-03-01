176653ed212363481929bdb2a3f42dd3f3c44b06
Fernando Isidro L <luffynando@gmail.com>
2022-03-01 16:26:54 -0600

    feat: migrate from php to node ComprobanteTotal

---
+++ src/cfdi33/standard/comprobante-total.ts
@@ -0,0 +1,29 @@
+import { AbstractDiscoverableVersion33 } from '../abstracts/abstract-discoverable-version33';
+import { ValidatorInterface } from '../../contracts/validator-interface';
+import { CNodeInterface } from '@nodecfdi/cfdiutils-common';
+import { Asserts } from '../../asserts';
+import { Status } from '../../status';
+
+/**
+ * ComprobanteTotal
+ *
+ * Válida que:
+ * - TOTAL01: El atributo Total existe, no está vacío y cumple con el patrón [0-9]+(.[0-9]+)?
+ */
+export class ComprobanteTotal extends AbstractDiscoverableVersion33 {
+    public validate(comprobante: CNodeInterface, asserts: Asserts): Promise<void> {
+        const pattern = /^[0-9]+(\.[0-9]+)?$/;
+        asserts.put(
+            'TOTAL01',
+            'El atributo Total existe, no está vacío y cumple con el patrón [0-9]+(.[0-9]+)?',
+            Status.when(
+                '' !== comprobante.attributes().get('Total') && !!comprobante.attributes().get('Total')?.match(pattern)
+            )
+        );
+        return Promise.resolve(undefined);
+    }
+
+    public static createDiscovered(): ValidatorInterface {
+        return new ComprobanteTotal();
+    }
+}
---
+++ tests/unit/cfdi33/standard/comprobante-total.test.ts
@@ -0,0 +1,34 @@
+import { useValidate33TestCase } from '../validate33-test-case';
+import { ComprobanteTotal } from '../../../../src/cfdi33/standard/comprobante-total';
+import { Status } from '../../../../src/status';
+
+describe('ComprobanteTotal', () => {
+    const { setValidator, getComprobante33, runValidate, assertStatusEqualsCode } = useValidate33TestCase();
+
+    beforeEach(() => {
+        setValidator(new ComprobanteTotal());
+    });
+
+    test.each([[''], [null], ['foo'], ['1.2e3'], ['0.'], ['.0'], ['0..0'], ['0.0.0'], ['-0.0001']])(
+        'total with invalid value',
+        async (value: string | null) => {
+            getComprobante33().addAttributes({
+                Total: value,
+            });
+
+            await runValidate();
+
+            assertStatusEqualsCode(Status.error(), 'TOTAL01');
+        }
+    );
+
+    test.each([['0'], ['0.0'], ['123.45']])('total with correct value', async (value: string | null) => {
+        getComprobante33().addAttributes({
+            Total: value,
+        });
+
+        await runValidate();
+
+        assertStatusEqualsCode(Status.ok(), 'TOTAL01');
+    });
+});
