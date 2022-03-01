ac8f2c77d717bbf4f1401a2b2eced5a29fdb7279
Fernando Isidro L <luffynando@gmail.com>
2022-03-01 15:31:35 -0600

    feat: migrate from php to node ComprobanteTipoCambio

---
+++ src/cfdi33/standard/comprobante-tipo-cambio.ts
@@ -0,0 +1,70 @@
+import { AbstractDiscoverableVersion33 } from '../abstracts/abstract-discoverable-version33';
+import { ValidatorInterface } from '../../contracts/validator-interface';
+import { Asserts } from '../../asserts';
+import { CNodeInterface } from '@nodecfdi/cfdiutils-common';
+import { Status } from '../../status';
+
+/**
+ * ComprobanteTipoCambio
+ *
+ * Válida que:
+ * - TIPOCAMBIO01: La moneda exista y no tenga un valor vacío
+ * - TIPOCAMBIO02: Si la moneda es "MXN", entonces el tipo de cambio debe tener el valor "1"
+ *                 o no debe existir (CFDI33113)
+ * - TIPOCAMBIO03: Si la moneda es "XXX", entonces el tipo de cambio no debe existir (CFDI33115)
+ * - TIPOCAMBIO04: Si la moneda no es "MXN" ni "XXX", entonces el tipo de cambio entonces
+ *                 el tipo de cambio debe seguir el patrón [0-9]{1,18}(.[0-9]{1,6})? (CFDI33114, CFDI33117)
+ */
+export class ComprobanteTipoCambio extends AbstractDiscoverableVersion33 {
+    private registerAssets(asserts: Asserts): void {
+        const assertDescriptions: Record<string, string> = {
+            TIPOCAMBIO01: 'La moneda exista y no tenga un valor vacío',
+            TIPOCAMBIO02: [
+                'Si la moneda es "MXN", entonces el tipo de cambio debe tener el valor "1"',
+                ' o no debe existir (CFDI33113)',
+            ].join(''),
+            TIPOCAMBIO03: 'Si la moneda es "XXX", entonces el tipo de cambio no debe existir (CFDI33115)',
+            TIPOCAMBIO04: [
+                'Si la moneda no es "MXN" ni "XXX", entonces el tipo de cambio',
+                ' debe seguir el patrón [0-9]{1,18}(.[0-9]{1,6}?) (CFDI33114, CFDI33117)',
+            ].join(''),
+        };
+        Object.entries(assertDescriptions).forEach(([code, title]) => {
+            asserts.put(code, title);
+        });
+    }
+
+    public validate(comprobante: CNodeInterface, asserts: Asserts): Promise<void> {
+        this.registerAssets(asserts);
+
+        const existsTipoCambio = comprobante.attributes().has('TipoCambio');
+        const tipoCambio = comprobante.attributes().get('TipoCambio');
+        const moneda = comprobante.attributes().get('Moneda');
+
+        asserts.putStatus('TIPOCAMBIO01', Status.when('' !== moneda));
+        if ('' === moneda) {
+            return Promise.resolve();
+        }
+
+        if ('MXN' === moneda) {
+            asserts.putStatus(
+                'TIPOCAMBIO02',
+                Status.when(!existsTipoCambio || Math.abs(parseFloat(tipoCambio || '0') - 1) < 0.0000001)
+            );
+        }
+
+        if ('XXX' === moneda) {
+            asserts.putStatus('TIPOCAMBIO03', Status.when(!existsTipoCambio));
+        }
+
+        if ('MXN' !== moneda && 'XXX' !== moneda) {
+            const pattern = /^[0-9]{1,18}(\.[0-9]{1,6})?$/;
+            asserts.putStatus('TIPOCAMBIO04', Status.when(!!tipoCambio?.match(pattern)));
+        }
+        return Promise.resolve(undefined);
+    }
+
+    public static createDiscovered(): ValidatorInterface {
+        return new ComprobanteTipoCambio();
+    }
+}
---
+++ tests/unit/cfdi33/standard/comprobante-tipo-cambio.test.ts
@@ -0,0 +1,91 @@
+import { ComprobanteTipoCambio } from '../../../../src/cfdi33/standard/comprobante-tipo-cambio';
+import { useValidate33TestCase } from '../validate33-test-case';
+import { Status } from '../../../../src/status';
+
+describe('ComprobanteTipoCambio', () => {
+    const { setValidator, getComprobante33, runValidate, assertStatusEqualsCode } = useValidate33TestCase();
+
+    beforeEach(() => {
+        setValidator(new ComprobanteTipoCambio());
+    });
+
+    test.each([
+        ['MXN', '1', 'TIPOCAMBIO02', ['TIPOCAMBIO03', 'TIPOCAMBIO04']],
+        ['MXN', '1.000000', 'TIPOCAMBIO02', ['TIPOCAMBIO03', 'TIPOCAMBIO04']],
+        ['MXN', null, 'TIPOCAMBIO02', ['TIPOCAMBIO03', 'TIPOCAMBIO04']],
+        ['XXX', null, 'TIPOCAMBIO03', ['TIPOCAMBIO02', 'TIPOCAMBIO04']],
+        ['USD', '10.0', 'TIPOCAMBIO04', ['TIPOCAMBIO02', 'TIPOCAMBIO03']],
+        ['USD', '20', 'TIPOCAMBIO04', ['TIPOCAMBIO02', 'TIPOCAMBIO03']],
+        ['USD', '0005.10000', 'TIPOCAMBIO04', ['TIPOCAMBIO02', 'TIPOCAMBIO03']],
+        ['USD', '123456789012345678.0', 'TIPOCAMBIO04', ['TIPOCAMBIO02', 'TIPOCAMBIO03']],
+        ['USD', '0.123456', 'TIPOCAMBIO04', ['TIPOCAMBIO02', 'TIPOCAMBIO03']],
+    ])('moneda with valid values', async (moneda: string, tipoCambio: string | null, ok: string, nones: string[]) => {
+        getComprobante33().addAttributes({
+            Moneda: moneda,
+            TipoCambio: tipoCambio,
+        });
+
+        await runValidate();
+
+        assertStatusEqualsCode(Status.ok(), 'TIPOCAMBIO01');
+        assertStatusEqualsCode(Status.ok(), ok);
+        nones.forEach((none) => {
+            assertStatusEqualsCode(Status.none(), none);
+        });
+    });
+
+    test.each([
+        [null, null],
+        [null, ''],
+        [null, '18.9000'],
+        ['', null],
+        ['', ''],
+        ['', '18.9000'],
+    ])('no moneda or empty', async (moneda: string | null, tipoCambio: string | null) => {
+        getComprobante33().addAttributes({
+            Moneda: moneda,
+            TipoCambio: tipoCambio,
+        });
+
+        await runValidate();
+
+        assertStatusEqualsCode(Status.error(), 'TIPOCAMBIO01');
+        assertStatusEqualsCode(Status.none(), 'TIPOCAMBIO02');
+        assertStatusEqualsCode(Status.none(), 'TIPOCAMBIO03');
+        assertStatusEqualsCode(Status.none(), 'TIPOCAMBIO04');
+    });
+
+    test.each([
+        ['MXN', '', 'TIPOCAMBIO02', ['TIPOCAMBIO03', 'TIPOCAMBIO04']],
+        ['MXN', '1.000001', 'TIPOCAMBIO02', ['TIPOCAMBIO03', 'TIPOCAMBIO04']],
+        ['MXN', '0.999999', 'TIPOCAMBIO02', ['TIPOCAMBIO03', 'TIPOCAMBIO04']],
+        ['MXN', '10.0', 'TIPOCAMBIO02', ['TIPOCAMBIO03', 'TIPOCAMBIO04']],
+        ['XXX', '', 'TIPOCAMBIO03', ['TIPOCAMBIO02', 'TIPOCAMBIO04']],
+        ['XXX', '10.0', 'TIPOCAMBIO03', ['TIPOCAMBIO02', 'TIPOCAMBIO04']],
+        ['USD', null, 'TIPOCAMBIO04', ['TIPOCAMBIO02', 'TIPOCAMBIO03']],
+        ['USD', '', 'TIPOCAMBIO04', ['TIPOCAMBIO02', 'TIPOCAMBIO03']],
+        ['USD', 'abc', 'TIPOCAMBIO04', ['TIPOCAMBIO02', 'TIPOCAMBIO03']],
+        ['USD', '1234567890123456789.0', 'TIPOCAMBIO04', ['TIPOCAMBIO02', 'TIPOCAMBIO03']],
+        ['USD', '0.1234567', 'TIPOCAMBIO04', ['TIPOCAMBIO02', 'TIPOCAMBIO03']],
+        ['USD', '0.', 'TIPOCAMBIO04', ['TIPOCAMBIO02', 'TIPOCAMBIO03']],
+        ['USD', '.0', 'TIPOCAMBIO04', ['TIPOCAMBIO02', 'TIPOCAMBIO03']],
+        ['USD', '0..0', 'TIPOCAMBIO04', ['TIPOCAMBIO02', 'TIPOCAMBIO03']],
+        ['USD', '0.0.0', 'TIPOCAMBIO04', ['TIPOCAMBIO02', 'TIPOCAMBIO03']],
+    ])(
+        'moneda with invalid values',
+        async (moneda: string, tipoCambio: string | null, error: string, nones: string[]) => {
+            getComprobante33().addAttributes({
+                Moneda: moneda,
+                TipoCambio: tipoCambio,
+            });
+
+            await runValidate();
+
+            assertStatusEqualsCode(Status.ok(), 'TIPOCAMBIO01');
+            assertStatusEqualsCode(Status.error(), error);
+            nones.forEach((none) => {
+                assertStatusEqualsCode(Status.none(), none);
+            });
+        }
+    );
+});
