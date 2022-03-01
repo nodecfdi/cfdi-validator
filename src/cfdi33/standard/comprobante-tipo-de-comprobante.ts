0c3b159eef07839c67b88acd1a9c23daa15b8c8d
Fernando Isidro L <luffynando@gmail.com>
2022-03-01 16:16:42 -0600

    feat: migrate from php to node ComprobanteTipoDeComprobante

---
+++ src/cfdi33/standard/comprobante-tipo-de-comprobante.ts
@@ -0,0 +1,131 @@
+import { AbstractDiscoverableVersion33 } from '../abstracts/abstract-discoverable-version33';
+import { ValidatorInterface } from '../../contracts/validator-interface';
+import { Asserts } from '../../asserts';
+import { CNodeInterface } from '@nodecfdi/cfdiutils-common';
+import { Status } from '../../status';
+
+/**
+ * ComprobanteTipoDeComprobante
+ *
+ * Válida que:
+ * - TIPOCOMP01: Si el tipo de comprobante es T, P ó N, entonces no debe existir las condiciones de pago
+ * - TIPOCOMP02: Si el tipo de comprobante es T, P ó N, entonces no debe existir la definición de impuestos (CFDI33179)
+ * - TIPOCOMP03: Si el tipo de comprobante es T ó P, entonces no debe existir la forma de pago
+ * - TIPOCOMP04: Si el tipo de comprobante es T ó P, entonces no debe existir el método de pago (CFDI33123)
+ * - TIPOCOMP05: Si el tipo de comprobante es T ó P, entonces no debe existir el descuento del comprobante (CFDI33110)
+ * - TIPOCOMP06: Si el tipo de comprobante es T ó P, entonces no debe existir el descuento de los conceptos (CFDI33179)
+ * - TIPOCOMP07: Si el tipo de comprobante es T ó P, entonces el subtotal debe ser cero (CFDI33108)
+ * - TIPOCOMP08: Si el tipo de comprobante es T ó P, entonces el total debe ser cero
+ * - TIPOCOMP09: Si el tipo de comprobante es I, E ó N, entonces el valor unitario de todos los conceptos
+ *               debe ser mayor que cero
+ * - TIPOCOMP010: Si el tipo de comprobante es N, entonces la moneda debe ser MXN
+ */
+export class ComprobanteTipoDeComprobante extends AbstractDiscoverableVersion33 {
+    private registerAsserts(asserts: Asserts): void {
+        const assertsDescriptions: Record<string, string> = {
+            TIPOCOMP01: [
+                'Si el tipo de comprobante es T, P ó N,',
+                ' entonces no debe existir las condiciones de pago',
+            ].join(''),
+            TIPOCOMP02: [
+                'Si el tipo de comprobante es T, P ó N,',
+                ' entonces no debe existir la definición de impuestos (CFDI33179)',
+            ].join(''),
+            TIPOCOMP03: 'Si el tipo de comprobante es T ó P, entonces no debe existir la forma de pago',
+
+            TIPOCOMP04: [
+                'Si el tipo de comprobante es T ó P,',
+                ' entonces no debe existir el método de pago (CFDI33123)',
+            ].join(''),
+            TIPOCOMP05: [
+                'Si el tipo de comprobante es T ó P,',
+                ' entonces no debe existir el descuento del comprobante (CFDI33110)',
+            ].join(''),
+            TIPOCOMP06: [
+                'Si el tipo de comprobante es T ó P,',
+                ' entonces no debe existir el descuento de los conceptos (CFDI33179)',
+            ].join(''),
+            TIPOCOMP07: 'Si el tipo de comprobante es T ó P, entonces el subtotal debe ser cero (CFDI33108)',
+            TIPOCOMP08: 'Si el tipo de comprobante es T ó P entonces el total debe ser cero',
+
+            TIPOCOMP09: [
+                'Si el tipo de comprobante es I, E ó N,',
+                ' entonces el valor unitario de todos los conceptos debe ser mayor que cero',
+            ].join(''),
+
+            TIPOCOMP10: 'Si el tipo de comprobante es N entonces, la moneda debe ser MXN',
+        };
+        Object.entries(assertsDescriptions).forEach(([code, title]) => {
+            asserts.put(code, title);
+        });
+    }
+
+    public validate(comprobante: CNodeInterface, asserts: Asserts): Promise<void> {
+        this.registerAsserts(asserts);
+
+        const tipoComprobante = comprobante.attributes().get('TipoDeComprobante');
+
+        if ('T' === tipoComprobante || 'P' === tipoComprobante || 'N' === tipoComprobante) {
+            asserts.putStatus('TIPOCOMP01', Status.when(!comprobante.attributes().has('CondicionesDePago')));
+            asserts.putStatus('TIPOCOMP02', Status.when(!comprobante.searchNode('cfdi:Impuestos')));
+        }
+
+        if ('T' === tipoComprobante || 'P' === tipoComprobante) {
+            asserts.putStatus('TIPOCOMP03', Status.when(!comprobante.attributes().has('FormaPago')));
+            asserts.putStatus('TIPOCOMP04', Status.when(!comprobante.attributes().has('MetodoPago')));
+            asserts.putStatus('TIPOCOMP05', Status.when(!comprobante.attributes().has('Descuento')));
+            asserts.putStatus('TIPOCOMP06', Status.when(this.checkConceptosDoesNotHaveDescuento(comprobante)));
+            asserts.putStatus('TIPOCOMP07', Status.when(this.isZero(comprobante.attributes().get('SubTotal'))));
+            asserts.putStatus('TIPOCOMP08', Status.when(this.isZero(comprobante.attributes().get('Total'))));
+        }
+
+        if ('I' === tipoComprobante || 'E' === tipoComprobante || 'N' === tipoComprobante) {
+            asserts.putStatus(
+                'TIPOCOMP09',
+                Status.when(this.checkConceptosValorUnitarioIsGreaterThanZero(comprobante))
+            );
+        }
+
+        if ('N' === tipoComprobante) {
+            asserts.putStatus('TIPOCOMP10', Status.when('MXN' === comprobante.attributes().get('Moneda')));
+        }
+
+        return Promise.resolve(undefined);
+    }
+
+    protected checkConceptosDoesNotHaveDescuento(comprobante: CNodeInterface): boolean {
+        for (const concepto of comprobante.searchNodes('cfdi:Conceptos', 'cfdi:Concepto')) {
+            if (concepto.attributes().has('Descuento')) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    protected checkConceptosValorUnitarioIsGreaterThanZero(comprobante: CNodeInterface): boolean {
+        for (const concepto of comprobante.searchNodes('cfdi:Conceptos', 'cfdi:Concepto')) {
+            if (!this.isGreaterThanZero(concepto.attributes().get('ValorUnitario'))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    protected isZero(value = ''): boolean {
+        if ('' === value || isNaN(Number(value))) {
+            return false;
+        }
+        return Math.abs(parseFloat(value)) < 0.0000001;
+    }
+
+    protected isGreaterThanZero(value = ''): boolean {
+        if ('' === value || isNaN(Number(value))) {
+            return false;
+        }
+        return Math.abs(parseFloat(value)) > 0.0000001;
+    }
+
+    public static createDiscovered(): ValidatorInterface {
+        return new ComprobanteTipoDeComprobante();
+    }
+}
---
+++ tests/unit/cfdi33/standard/comprobante-tipo-de-comprobante.test.ts
@@ -0,0 +1,166 @@
+import { useValidate33TestCase } from '../validate33-test-case';
+import { ComprobanteTipoDeComprobante } from '../../../../src/cfdi33/standard/comprobante-tipo-de-comprobante';
+import { Status } from '../../../../src/status';
+import { CNode } from '@nodecfdi/cfdiutils-common';
+
+describe('ComprobanteTipoDeComprobante', () => {
+    const { setValidator, getComprobante33, runValidate, assertStatusEqualsCode } = useValidate33TestCase();
+
+    beforeEach(() => {
+        setValidator(new ComprobanteTipoDeComprobante());
+    });
+
+    test.each([['T'], ['P'], ['N']])('valid TPN', async (tipoDeComprobante) => {
+        getComprobante33().addAttributes({
+            TipoDeComprobante: tipoDeComprobante,
+        });
+
+        await runValidate();
+
+        assertStatusEqualsCode(Status.ok(), 'TIPOCOMP01');
+        assertStatusEqualsCode(Status.ok(), 'TIPOCOMP02');
+    });
+
+    test.each([['T'], ['P'], ['N']])('invalid TPN', async (tipoDeComprobante) => {
+        getComprobante33().addAttributes({
+            TipoDeComprobante: tipoDeComprobante,
+            CondicionesDePago: '',
+            FormaPago: '',
+            MetodoPago: '',
+        });
+        getComprobante33().addChild(new CNode('cfdi:Impuestos'));
+
+        await runValidate();
+
+        assertStatusEqualsCode(Status.error(), 'TIPOCOMP01');
+        assertStatusEqualsCode(Status.error(), 'TIPOCOMP02');
+    });
+
+    test.each([['T'], ['P']])('valid TP', async (tipoDeComprobante) => {
+        getComprobante33().addAttributes({
+            TipoDeComprobante: tipoDeComprobante,
+            FormaPago: null, // set to null to make clear that it must not exist
+            MetodoPago: null, // set to null to make clear that it must not exist
+            SubTotal: '0',
+            Total: '0.00',
+        });
+
+        await runValidate();
+
+        assertStatusEqualsCode(Status.ok(), 'TIPOCOMP03');
+        assertStatusEqualsCode(Status.ok(), 'TIPOCOMP04');
+        assertStatusEqualsCode(Status.ok(), 'TIPOCOMP05');
+        assertStatusEqualsCode(Status.ok(), 'TIPOCOMP06');
+        assertStatusEqualsCode(Status.ok(), 'TIPOCOMP07');
+        assertStatusEqualsCode(Status.ok(), 'TIPOCOMP08');
+    });
+
+    test.each([['T'], ['P']])('invalid TP', async (tipoDeComprobante) => {
+        getComprobante33().addAttributes({
+            TipoDeComprobante: tipoDeComprobante,
+            FormaPago: '',
+            MetodoPago: '',
+        });
+
+        await runValidate();
+
+        assertStatusEqualsCode(Status.error(), 'TIPOCOMP03');
+        assertStatusEqualsCode(Status.error(), 'TIPOCOMP04');
+    });
+
+    test.each([['T'], ['P']])('invalid TP descuentos', async (tipoDeComprobante) => {
+        getComprobante33().addAttributes({
+            TipoDeComprobante: tipoDeComprobante,
+            Descuento: '',
+        });
+        getComprobante33().addChild(
+            new CNode('cfdi:Conceptos', {}, [new CNode('cfdi:Concepto'), new CNode('cfdi:Concepto', { Descuento: '' })])
+        );
+
+        await runValidate();
+
+        assertStatusEqualsCode(Status.error(), 'TIPOCOMP05');
+        assertStatusEqualsCode(Status.error(), 'TIPOCOMP06');
+    });
+
+    const providerToNonZero = (): (string | null)[][] => {
+        const types = ['T', 'P'];
+        const values = [null, '', '0.000001', '123.45', 'foo'];
+        const provider: (string | null)[][] = [];
+        types.forEach((tipoDeComprobante) => {
+            values.forEach((subtotal) => {
+                provider.push([tipoDeComprobante, subtotal]);
+            });
+        });
+        return provider;
+    };
+
+    test.each(providerToNonZero())('invalid subtotal', async (tipoDeComprobante, subtotal) => {
+        getComprobante33().addAttributes({
+            TipoDeComprobante: tipoDeComprobante,
+            SubTotal: subtotal,
+        });
+
+        await runValidate();
+
+        assertStatusEqualsCode(Status.error(), 'TIPOCOMP07');
+    });
+
+    test.each(providerToNonZero())('invalid total', async (tipoDeComprobante, total) => {
+        getComprobante33().addAttributes({
+            TipoDeComprobante: tipoDeComprobante,
+            Total: total,
+        });
+
+        await runValidate();
+
+        assertStatusEqualsCode(Status.error(), 'TIPOCOMP08');
+    });
+
+    test.each([['I'], ['E'], ['N']])('valid IEN valor unitario greater than zero %s', async (tipoDeComprobante) => {
+        getComprobante33().addAttributes({
+            TipoDeComprobante: tipoDeComprobante,
+        });
+        getComprobante33().addChild(
+            new CNode('cfdi:Conceptos', {}, [
+                new CNode('cfdi:Concepto', { ValorUnitario: '123.45' }),
+                new CNode('cfdi:Concepto', { ValorUnitario: '0.000001' }),
+            ])
+        );
+
+        await runValidate();
+
+        assertStatusEqualsCode(Status.ok(), 'TIPOCOMP09');
+    });
+
+    const providerIENWrongValue = (): (string | null)[][] => {
+        const tC = ['I', 'E', 'N'];
+        const wrongUnits = [null, '', '0', '0.00', '0.0000001'];
+        const provider: (string | null)[][] = [];
+        tC.forEach((tipoDeComprobante) => {
+            wrongUnits.forEach((wrongUnit) => {
+                provider.push([tipoDeComprobante, wrongUnit]);
+            });
+        });
+        return provider;
+    };
+
+    test.each(providerIENWrongValue())(
+        'invalid IEN valor unitario greater than zero',
+        async (tipoDeComprobante, wrongUnitValue) => {
+            getComprobante33().addAttributes({
+                TipoDeComprobante: tipoDeComprobante,
+            });
+            getComprobante33().addChild(
+                new CNode('cfdi:Conceptos', {}, [
+                    new CNode('cfdi:Concepto', { ValorUnitario: '123.45' }),
+                    new CNode('cfdi:Concepto', { ValorUnitario: wrongUnitValue }),
+                ])
+            );
+
+            await runValidate();
+
+            assertStatusEqualsCode(Status.error(), 'TIPOCOMP09');
+        }
+    );
+});
