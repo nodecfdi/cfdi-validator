5c65b10b42759dd75e50dc296b4b98d8fb52340a
Fernando Isidro L <luffynando@gmail.com>
2022-02-28 21:18:26 -0600

    feat: migrate from php to node ComprobantePagos

---
+++ src/cfdi33/recepcion-pagos/comprobante-pagos.ts
@@ -0,0 +1,84 @@
+import { AbstractRecepcionPagos10 } from '../abstracts/abstract-recepcion-pagos10';
+import { ValidatorInterface } from '../../contracts/validator-interface';
+import { CNodeInterface } from '@nodecfdi/cfdiutils-common';
+import { Asserts } from '../../asserts';
+import { Status } from '../../status';
+
+/**
+ * ComprobantePagos - Válida los datos relacionados con el nodo Comprobante cuando es un CFDI de recepción de pagos
+ *
+ * - PAGCOMP01: Debe existir un solo nodo que represente el complemento de pagos
+ * - PAGCOMP02: La forma de pago no debe existir (CRP104)
+ * - PAGCOMP03: Las condiciones de pago no deben existir (CRP106)
+ * - PAGCOMP04: El método de pago no deben existir (CRP105)
+ * - PAGCOMP05: La moneda debe ser "XXX" (CRP103)
+ * - PAGCOMP06: El tipo de cambio no debe existir (CRP108)
+ * - PAGCOMP07: El descuento no debe existir (CRP107)
+ * - PAGCOMP08: El subtotal del documento debe ser cero "0" (CRP102)
+ * - PAGCOMP09: El total del documento debe ser cero "0" (CRP109)
+ * - PAGCOMP10: No se debe registrar el apartado de Impuestos en el CFDI (CRP122)
+ */
+export class ComprobantePagos extends AbstractRecepcionPagos10 {
+    public validateRecepcionPagos(comprobante: CNodeInterface, asserts: Asserts): Promise<void> {
+        const pagos = comprobante.searchNodes('cfdi:Complemento', 'pago10:Pagos');
+        asserts.put(
+            'PAGCOMP01',
+            'Debe existir un solo nodo que represente el complemento de pagos',
+            Status.when(1 === pagos.length),
+            `Encontrados: ${pagos.length}`
+        );
+        asserts.put(
+            'PAGCOMP02',
+            'La forma de pago no debe existir (CRP104)',
+            Status.when(!comprobante.attributes().has('FormaPago'))
+        );
+        asserts.put(
+            'PAGCOMP03',
+            'Las condiciones de pago no deben existir (CRP106)',
+            Status.when(!comprobante.attributes().has('CondicionesDePago'))
+        );
+        asserts.put(
+            'PAGCOMP04',
+            'El método de pago no debe existir (CRP105)',
+            Status.when(!comprobante.attributes().has('MetodoPago'))
+        );
+        asserts.put(
+            'PAGCOMP05',
+            'La moneda debe ser "XXX" (CRP103)',
+            Status.when('XXX' === comprobante.attributes().get('Moneda')),
+            `Moneda: "${comprobante.attributes().get('Moneda')}"`
+        );
+        asserts.put(
+            'PAGCOMP06',
+            'El tipo de cambio no debe existir (CRP108)',
+            Status.when(!comprobante.attributes().has('TipoCambio'))
+        );
+        asserts.put(
+            'PAGCOMP07',
+            'El descuento no debe existir (CRP107)',
+            Status.when(!comprobante.attributes().has('Descuento'))
+        );
+        asserts.put(
+            'PAGCOMP08',
+            'El subtotal del documento debe ser cero "0" (CRP102)',
+            Status.when('0' === comprobante.attributes().get('SubTotal')),
+            `SubTotal: "${comprobante.attributes().get('SubTotal')}"`
+        );
+        asserts.put(
+            'PAGCOMP09',
+            'El total del documento debe ser cero "0" (CRP109)',
+            Status.when('0' === comprobante.attributes().get('Total')),
+            `Total: "${comprobante.attributes().get('Total')}"`
+        );
+        asserts.put(
+            'PAGCOMP10',
+            'No se debe registrar el apartado de Impuesto en el CFDI (CRP122)',
+            Status.when(0 === comprobante.searchNodes('cfdi:Impuestos').length)
+        );
+        return Promise.resolve();
+    }
+
+    public static createDiscovered(): ValidatorInterface {
+        return new ComprobantePagos();
+    }
+}
---
+++ tests/unit/cfdi33/recepcion-pagos/comprobante-pagos.test.ts
@@ -0,0 +1,115 @@
+import { useValidateComplementoPagosTestCase } from './validate-complemento-pagos-test-case';
+import { ComprobantePagos } from '../../../../src/cfdi33/recepcion-pagos/comprobante-pagos';
+import { Status } from '../../../../src/status';
+import { Pagos10 } from '@nodecfdi/cfdiutils-elements';
+
+describe('ComprobantePagos', () => {
+    const { setValidator, getComprobante33, runValidate, assertStatusEqualsCode } =
+        useValidateComplementoPagosTestCase();
+
+    beforeEach(() => {
+        setValidator(new ComprobantePagos());
+
+        // setup a valid case and in the test change to make it fail
+        const comprobante = getComprobante33();
+        comprobante.addAttributes({
+            Moneda: 'XXX',
+            SubTotal: '0',
+            Total: '0',
+        });
+    });
+
+    test('valid case', async () => {
+        await runValidate();
+
+        for (let i = 1; i <= 10; i++) {
+            assertStatusEqualsCode(Status.ok(), `PAGCOMP${i.toString().padStart(2, '0')}`);
+        }
+    });
+
+    test('error with more than one complemento pagos', async () => {
+        getComprobante33().getComplemento().add(new Pagos10.Pagos());
+        await runValidate();
+
+        assertStatusEqualsCode(Status.error(), 'PAGCOMP01');
+    });
+
+    test('error with forma pago', async () => {
+        getComprobante33().addAttributes({
+            FormaPago: '', // exists, even empty
+        });
+        await runValidate();
+
+        assertStatusEqualsCode(Status.error(), 'PAGCOMP02');
+    });
+
+    test('error with condiciones de pago', async () => {
+        getComprobante33().addAttributes({
+            CondicionesDePago: '', // exists, even empty
+        });
+        await runValidate();
+
+        assertStatusEqualsCode(Status.error(), 'PAGCOMP03');
+    });
+
+    test('error with metodo pago', async () => {
+        getComprobante33().addAttributes({
+            MetodoPago: '', // exists, even empty
+        });
+        await runValidate();
+
+        assertStatusEqualsCode(Status.error(), 'PAGCOMP04');
+    });
+
+    test.each([[''], [null], ['MXN']])('error with moneda not XXX', async (input: string | null) => {
+        getComprobante33().addAttributes({
+            Moneda: input,
+        });
+        await runValidate();
+
+        assertStatusEqualsCode(Status.error(), 'PAGCOMP05');
+    });
+
+    test('error with tipo cambio', async () => {
+        getComprobante33().addAttributes({
+            TipoCambio: '', // exists, even empty
+        });
+        await runValidate();
+
+        assertStatusEqualsCode(Status.error(), 'PAGCOMP06');
+    });
+
+    test('error with descuento', async () => {
+        getComprobante33().addAttributes({
+            Descuento: '', // exists, even empty
+        });
+        await runValidate();
+
+        assertStatusEqualsCode(Status.error(), 'PAGCOMP07');
+    });
+
+    test.each([[''], [null], ['0.0']])('error with subtotal not zero', async (input: string | null) => {
+        getComprobante33().addAttributes({
+            SubTotal: input,
+        });
+        await runValidate();
+
+        assertStatusEqualsCode(Status.error(), 'PAGCOMP08');
+    });
+
+    test.each([[''], [null], ['0.0']])('error with total not zero', async (input: string | null) => {
+        getComprobante33().addAttributes({
+            Total: input,
+        });
+        await runValidate();
+
+        assertStatusEqualsCode(Status.error(), 'PAGCOMP09');
+    });
+
+    test('error with impuestos', async () => {
+        getComprobante33().getImpuestos();
+        await runValidate();
+
+        assertStatusEqualsCode(Status.error(), 'PAGCOMP10');
+    });
+});
