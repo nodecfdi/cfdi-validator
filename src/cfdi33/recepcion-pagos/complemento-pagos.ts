25e9fb8c52c0daacfe259a5ca8657433ce0a60a5
Fernando Isidro L <luffynando@gmail.com>
2022-02-28 21:01:36 -0600

    feat: migrate from php to node ComplementoPagos

---
+++ src/cfdi33/recepcion-pagos/complemento-pagos.ts
@@ -0,0 +1,56 @@
+/**
+ * ComplementoPagos
+ *
+ * Este complemento se ejecuta siempre
+ *
+ * - COMPPAG01: El complemento de pagos debe existir si el tipo de comprobante es P y viceversa
+ * - COMPPAG02: Si el complemento de pagos existe su version debe ser 1.0
+ * - COMPPAG03: Si el tipo de comprobante es P su versión debe ser 3.3
+ * - COMPPAG04: No debe existir el nodo impuestos del complemento de pagos (CRP237)
+ */
+import { AbstractDiscoverableVersion33 } from '../abstracts/abstract-discoverable-version33';
+import { CNode, CNodeInterface } from '@nodecfdi/cfdiutils-common';
+import { Asserts } from '../../asserts';
+import { Status } from '../../status';
+import { ValidatorInterface } from '../../contracts/validator-interface';
+import { ComprobantePagos } from './comprobante-pagos';
+
+export class ComplementoPagos extends AbstractDiscoverableVersion33 {
+    public validate(comprobante: CNodeInterface, asserts: Asserts): Promise<void> {
+        asserts.put('COMPPAG01', 'El complemento de pagos debe existir si el tipo de comprobante es P y viceversa');
+        asserts.put('COMPPAG02', 'Si el complemento de pagos existe su version debe ser 1.0');
+        asserts.put('COMPPAG03', 'Si el tipo de comprobante es P su versión debe ser 3.3');
+        asserts.put('COMPPAG04', 'No debe existir el nodo impuestos del complemento de pagos (CRP237)');
+
+        let pagosExists = true;
+        let pagos10 = comprobante.searchNode('cfdi:Complemento', 'pago10:Pagos');
+        if (!pagos10) {
+            pagosExists = false;
+            pagos10 = new CNode('pago10:Pagos'); // avoid accessing a null object
+        }
+
+        const isTipoPago = 'P' === comprobante.attributes().get('TipoDeComprobante');
+
+        asserts.putStatus(
+            'COMPPAG01',
+            Status.when(!(isTipoPago ? !pagosExists : pagosExists)),
+            `Tipo de comprobante: "${comprobante.attributes().get('TipoDeComprobante')}", Complemento: "${
+                pagosExists ? 'existe' : 'no existe'
+            }"`
+        );
+
+        if (pagosExists) {
+            asserts.putStatus('COMPPAG02', Status.when('1.0' === pagos10.attributes().get('Version')));
+        }
+        if (isTipoPago) {
+            asserts.putStatus('COMPPAG03', Status.when('3.3' === comprobante.attributes().get('Version')));
+        }
+
+        asserts.putStatus('COMPPAG04', Status.when(!pagos10.searchNode('pago10:Impuestos')));
+        return Promise.resolve(undefined);
+    }
+
+    public static createDiscovered(): ValidatorInterface {
+        return new ComprobantePagos();
+    }
+}
---
+++ tests/unit/cfdi33/recepcion-pagos/complemento-pagos.test.ts
@@ -0,0 +1,103 @@
+import { useValidate33TestCase } from '../validate33-test-case';
+import { ComplementoPagos } from '../../../../src/cfdi33/recepcion-pagos/complemento-pagos';
+import { Pagos10 } from '@nodecfdi/cfdiutils-elements';
+import { Status } from '../../../../src/status';
+import { CNode } from '@nodecfdi/cfdiutils-common';
+
+describe('ComplementoPagos', () => {
+    const { setValidator, getComprobante33, runValidate, assertStatusEqualsCode } = useValidate33TestCase();
+
+    const setUpComplemento = (): Pagos10.Pagos => {
+        const comprobante = getComprobante33();
+        comprobante.attributes().set('TipoDeComprobante', 'P');
+
+        const pagos = new Pagos10.Pagos();
+        comprobante.addComplemento(pagos);
+
+        return pagos;
+    };
+
+    beforeEach(() => {
+        setValidator(new ComplementoPagos());
+    });
+
+    test('valid case with complemento', async () => {
+        setUpComplemento();
+
+        await runValidate();
+
+        assertStatusEqualsCode(Status.ok(), 'COMPPAG01');
+        assertStatusEqualsCode(Status.ok(), 'COMPPAG02');
+        assertStatusEqualsCode(Status.ok(), 'COMPPAG03');
+    });
+
+    test('valid case without complemento', async () => {
+        await runValidate();
+
+        assertStatusEqualsCode(Status.ok(), 'COMPPAG01');
+        assertStatusEqualsCode(Status.none(), 'COMPPAG02');
+        assertStatusEqualsCode(Status.none(), 'COMPPAG03');
+    });
+
+    test('without complemento', async () => {
+        const comprobante = getComprobante33();
+        comprobante.attributes().set('TipoDeComprobante', 'P');
+
+        await runValidate();
+
+        assertStatusEqualsCode(Status.error(), 'COMPPAG01');
+        assertStatusEqualsCode(Status.none(), 'COMPPAG02');
+        assertStatusEqualsCode(Status.ok(), 'COMPPAG03');
+    });
+
+    test('without tipo de comprobante', async () => {
+        const comprobante = getComprobante33();
+        comprobante.addComplemento(new Pagos10.Pagos());
+
+        await runValidate();
+
+        assertStatusEqualsCode(Status.error(), 'COMPPAG01');
+        assertStatusEqualsCode(Status.ok(), 'COMPPAG02');
+        assertStatusEqualsCode(Status.none(), 'COMPPAG03');
+    });
+
+    test('with invalid comprobante version', async () => {
+        setUpComplemento();
+
+        getComprobante33().attributes().set('Version', '3.2');
+
+        await runValidate();
+
+        assertStatusEqualsCode(Status.ok(), 'COMPPAG01');
+        assertStatusEqualsCode(Status.ok(), 'COMPPAG02');
+        assertStatusEqualsCode(Status.error(), 'COMPPAG03');
+    });
+
+    test('with invalid complemento version', async () => {
+        const complemento = setUpComplemento();
+        complemento.attributes().set('Version', '0.9');
+
+        await runValidate();
+
+        assertStatusEqualsCode(Status.ok(), 'COMPPAG01');
+        assertStatusEqualsCode(Status.error(), 'COMPPAG02');
+        assertStatusEqualsCode(Status.ok(), 'COMPPAG03');
+    });
+
+    test('impuestos must not exists', async () => {
+        setUpComplemento();
+
+        await runValidate();
+
+        assertStatusEqualsCode(Status.ok(), 'COMPPAG04');
+    });
+
+    test('impuestos must not exists but exists', async () => {
+        const pagos = setUpComplemento();
+        pagos.addChild(new CNode('pago10:Impuestos'));
+
+        await runValidate();
+
+        assertStatusEqualsCode(Status.error(), 'COMPPAG04');
+    });
+});
