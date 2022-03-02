b056c9983eac324fb9606be130f683dea3afa6d6
Fernando Isidro L <luffynando@gmail.com>
2022-03-01 19:26:55 -0600

    feat: migrate from php to node EmisorRfc

---
+++ src/cfdi33/standard/emisor-rfc.ts
@@ -0,0 +1,36 @@
+import { AbstractDiscoverableVersion33 } from '../abstracts/abstract-discoverable-version33';
+import { ValidatorInterface } from '../../contracts/validator-interface';
+import { CNodeInterface } from '@nodecfdi/cfdiutils-common';
+import { Asserts } from '../../asserts';
+import { Rfc } from '@nodecfdi/rfc';
+import { Status } from '../../status';
+
+/**
+ * EmisorRfc
+ *
+ * Válida que:
+ *  - EMISORRFC01: El RFC del emisor del comprobante debe ser válido y diferente de XAXX010101000 y XEXX010101000
+ */
+export class EmisorRfc extends AbstractDiscoverableVersion33 {
+    public validate(comprobante: CNodeInterface, asserts: Asserts): Promise<void> {
+        const assert = asserts.put(
+            'EMISORRFC01',
+            'El RFC del emisor del comprobante debe ser válido y diferente de XAXX010101000 y XEXX010101000'
+        );
+
+        const emisorRfc = comprobante.searchAttribute('cfdi:Emisor', 'Rfc');
+        try {
+            Rfc.checkIsValid(emisorRfc, Rfc.DISALLOW_FOREIGN | Rfc.DISALLOW_GENERIC);
+        } catch (e) {
+            assert.setStatus(Status.error(), `Rfc: "${emisorRfc}". ${(e as Error).message}`);
+            return Promise.resolve(undefined);
+        }
+        assert.setStatus(Status.ok());
+
+        return Promise.resolve(undefined);
+    }
+
+    public static createDiscovered(): ValidatorInterface {
+        return new EmisorRfc();
+    }
+}
---
+++ tests/unit/cfdi33/standard/emisor-rfc.test.ts
@@ -0,0 +1,47 @@
+import { useValidate33TestCase } from '../validate33-test-case';
+import { EmisorRfc } from '../../../../src/cfdi33/standard/emisor-rfc';
+import { CNode } from '@nodecfdi/cfdiutils-common';
+import { Status } from '../../../../src/status';
+import { Rfc } from '@nodecfdi/rfc';
+
+describe('Emisor Rfc', () => {
+    const { setValidator, runValidate, getComprobante33, getAsserts, assertStatusEqualsCode } = useValidate33TestCase();
+
+    beforeEach(() => {
+        setValidator(new EmisorRfc());
+    });
+
+    test.each([
+        ['person', 'COSC8001137NA'],
+        ['moral', 'DIM8701081LA'],
+    ])('valid cases %s', async (name: string, rfc: string) => {
+        getComprobante33().addChild(
+            new CNode('cfdi:Emisor', {
+                Rfc: rfc,
+            })
+        );
+
+        await runValidate();
+
+        expect(getAsserts().hasErrors()).toBeFalsy();
+        assertStatusEqualsCode(Status.ok(), 'EMISORRFC01');
+    });
+
+    test.each([
+        ['none', null],
+        ['empty', ''],
+        ['wrong', 'COSC8099137NA'],
+        ['generic', Rfc.RFC_GENERIC],
+        ['foreign', Rfc.RFC_FOREIGN],
+    ])('valid cases %s', async (name: string, rfc: string | null) => {
+        getComprobante33().addChild(
+            new CNode('cfdi:Emisor', {
+                Rfc: rfc,
+            })
+        );
+
+        await runValidate();
+
+        assertStatusEqualsCode(Status.error(), 'EMISORRFC01');
+    });
+});
