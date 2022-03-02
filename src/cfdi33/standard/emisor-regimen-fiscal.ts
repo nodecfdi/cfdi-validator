eb778ff63b202e9257f57e207c5f6f8124bf1827
Fernando Isidro L <luffynando@gmail.com>
2022-03-01 19:21:32 -0600

    feat: migrate from php to node EmisorRegimenFiscal

---
+++ src/cfdi33/standard/emisor-regimen-fiscal.ts
@@ -0,0 +1,59 @@
+import { AbstractDiscoverableVersion33 } from '../abstracts/abstract-discoverable-version33';
+import { ValidatorInterface } from '../../contracts/validator-interface';
+import { CNodeInterface } from '@nodecfdi/cfdiutils-common';
+import { Asserts } from '../../asserts';
+import { Status } from '../../status';
+
+/**
+ * EmisorRegimenFiscal
+ *
+ * Válida que:
+ *  - REGFIS01: El régimen fiscal contenga un valor apropiado según el tipo de RFC emisor (CFDI33130 y CFDI33131)
+ *
+ * Nota: No válida que el RFC sea válido, esa responsabilidad no es de este validador.
+ */
+export class EmisorRegimenFiscal extends AbstractDiscoverableVersion33 {
+    public validate(comprobante: CNodeInterface, asserts: Asserts): Promise<void> {
+        const regimenFiscal = comprobante.searchAttribute('cfdi:Emisor', 'RegimenFiscal');
+        const emisorRfc = comprobante.searchAttribute('cfdi:Emisor', 'Rfc');
+
+        const length = emisorRfc.length;
+        let validCodes: string[];
+
+        if (12 === length) {
+            validCodes = ['601', '603', '609', '620', '623', '624', '628', '607', '610', '622', '626'];
+        } else if (13 === length) {
+            validCodes = [
+                '605',
+                '606',
+                '608',
+                '611',
+                '612',
+                '614',
+                '616',
+                '621',
+                '629',
+                '630',
+                '615',
+                '610',
+                '622',
+                '626',
+            ];
+        } else {
+            validCodes = [];
+        }
+
+        asserts.put(
+            'REGFIS01',
+            'El régimen fiscal contenga un valor apropiado según el tipo de RFC emisor (CFDI33130 y CFDI33131)',
+            Status.when(validCodes.includes(regimenFiscal)),
+            `Rfc: "${emisorRfc}", Regimen Fiscal: "${regimenFiscal}"`
+        );
+
+        return Promise.resolve(undefined);
+    }
+
+    public static createDiscovered(): ValidatorInterface {
+        return new EmisorRegimenFiscal();
+    }
+}
---
+++ tests/unit/cfdi33/standard/emisor-regimen-fiscal.test.ts
@@ -0,0 +1,78 @@
+import { useValidate33TestCase } from '../validate33-test-case';
+import { EmisorRegimenFiscal } from '../../../../src/cfdi33/standard/emisor-regimen-fiscal';
+import { CNode } from '@nodecfdi/cfdiutils-common';
+import { Status } from '../../../../src/status';
+
+describe('EmisorRegimenFiscal', () => {
+    const { setValidator, getComprobante33, runValidate, assertStatusEqualsCode } = useValidate33TestCase();
+
+    beforeEach(() => {
+        setValidator(new EmisorRegimenFiscal());
+    });
+
+    test.each([
+        // personas morales
+        ['AAA010101AAA', '601'],
+        ['AAA010101AAA', '603'],
+        ['AAA010101AAA', '609'],
+        ['AAA010101AAA', '610'],
+        ['AAA010101AAA', '620'],
+        ['AAA010101AAA', '622'],
+        ['AAA010101AAA', '623'],
+        ['AAA010101AAA', '624'],
+        ['AAAA010101AA', '626'],
+        ['AAA010101AAA', '628'],
+        ['AAA010101AAA', '607'],
+        ['ÑAA010101AAA', '601'], // with Ñ
+        // personas físicas
+        ['AAAA010101AAA', '605'],
+        ['AAAA010101AAA', '606'],
+        ['AAAA010101AAA', '608'],
+        ['AAAA010101AAA', '610'],
+        ['AAAA010101AAA', '611'],
+        ['AAAA010101AAA', '612'],
+        ['AAAA010101AAA', '614'],
+        ['AAAA010101AAA', '616'],
+        ['AAAA010101AAA', '621'],
+        ['AAAA010101AAA', '622'],
+        ['AAAA010101AAA', '629'],
+        ['AAAA010101AAA', '630'],
+        ['AAAA010101AAA', '626'], // regimen RESICO
+        ['AAAA010101AAA', '615'],
+        ['ÑAAA010101AAA', '605'], // with Ñ
+        ['AAA010000AAA', '601'], // RFC inválido, regimen válido persona moral
+        ['AAAA010000AAA', '605'], // RFC inválido, regimen válido persona física
+    ])('valid cases', async (emisorRfc, regimenFiscal) => {
+        getComprobante33().addChild(
+            new CNode('cfdi:Emisor', {
+                RegimenFiscal: regimenFiscal,
+                Rfc: emisorRfc,
+            })
+        );
+
+        await runValidate();
+
+        assertStatusEqualsCode(Status.ok(), 'REGFIS01');
+    });
+
+    test.each([
+        ['AAA010101AAA', '605'], // persona moral con regimen incorrecto
+        ['AAAA010101AAA', '601'], // persona física con regimen incorrecto
+        ['', '615'], // RFC vacío, con regimen
+        ['', ''], // RFC vacío, regimen vacío
+        [null, ''], // sin RFC, regimen vacío
+        [null, '630'], // sin RFC, con regimen
+        [null, null], // sin RFC, sin regimen
+    ])('invalid cases', async (emisorRfc: string | null, regimenFiscal: string | null) => {
+        getComprobante33().addChild(
+            new CNode('cfdi:Emisor', {
+                RegimenFiscal: regimenFiscal,
+                Rfc: emisorRfc,
+            })
+        );
+
+        await runValidate();
+
+        assertStatusEqualsCode(Status.error(), 'REGFIS01');
+    });
+});
