f2883c74e16bfe0a4eae34cd7513079bc36bd4f1
Fernando Isidro L <luffynando@gmail.com>
2022-03-01 20:02:43 -0600

    feat: migrate from php to node FechaComprobante

---
+++ src/cfdi33/standard/fecha-comprobante.ts
@@ -0,0 +1,85 @@
+import { AbstractDiscoverableVersion33 } from '../abstracts/abstract-discoverable-version33';
+import { ValidatorInterface } from '../../contracts/validator-interface';
+import { DateTime } from 'luxon';
+import { CNodeInterface } from '@nodecfdi/cfdiutils-common';
+import { Asserts } from '../../asserts';
+import { AssertFechaFormat } from '../utils/assert-fecha-format';
+import { Status } from '../../status';
+
+/**
+ * FechaComprobante
+ *
+ * Válida que:
+ * - FECHA01: La fecha del comprobante cumple con el formato
+ * - FECHA02: La fecha existe en el comprobante y es mayor que 2017-07-01 y menor que el futuro
+ *      - La fecha en el futuro se puede configurar a un valor determinado
+ *      - La fecha en el futuro es por defecto el momento de validación más una tolerancia
+ *      - La tolerancia puede ser configurada y es por defecto 300 segundos
+ */
+export class FechaComprobante extends AbstractDiscoverableVersion33 {
+    private _maximumDate?: number;
+
+    /** Tolerancia en segundos */
+    private _tolerance = 300;
+
+    public getMinimumDate(): number {
+        return DateTime.fromObject({
+            hour: 0,
+            minute: 0,
+            second: 0,
+            month: 7,
+            day: 1,
+            year: 2017,
+        }).toMillis();
+    }
+
+    public getMaximumDate(): number {
+        if (this._maximumDate === undefined || isNaN(Number(this._maximumDate))) {
+            return Date.now() + this.getTolerance();
+        }
+        return this._maximumDate;
+    }
+
+    public setMaximumDate(maximumDate: number | null = null): void {
+        this._maximumDate = maximumDate !== null ? maximumDate : undefined;
+    }
+
+    public getTolerance(): number {
+        return this._tolerance;
+    }
+
+    public setTolerance(tolerance: number): void {
+        this._tolerance = tolerance;
+    }
+
+    public validate(comprobante: CNodeInterface, asserts: Asserts): Promise<void> {
+        const fechaSource = comprobante.attributes().get('Fecha');
+        const hasFormat = AssertFechaFormat.assertFormat(asserts, 'FECHA01', 'del comprobante', fechaSource || '');
+        const assertBetween = asserts.put(
+            'FECHA02',
+            'La fecha existe en el comprobante y es mayor que 2017-07-01 y menor que el futuro'
+        );
+        if (!hasFormat) {
+            return Promise.resolve();
+        }
+
+        const exists = comprobante.attributes().has('Fecha');
+        const testDate = '' !== fechaSource ? DateTime.fromISO(fechaSource || '').toMillis() : 0;
+
+        const minimumDate = this.getMinimumDate();
+        const maximumDate = this.getMaximumDate();
+
+        assertBetween.setStatus(
+            Status.when(testDate >= minimumDate && testDate <= maximumDate),
+            `Fecha: "${fechaSource}" (${exists ? 'Existe' : 'No existe'}), Máxima: ${DateTime.fromMillis(
+                maximumDate
+            ).toFormat('yyyy-LL-dd HH:mm:ss')}`
+        );
+
+        return Promise.resolve(undefined);
+    }
+
+    public static createDiscovered(): ValidatorInterface {
+        return new FechaComprobante();
+    }
+}
---
+++ tests/unit/cfdi33/standard/fecha-comprobante.test.ts
@@ -0,0 +1,127 @@
+import { useValidate33TestCase } from '../validate33-test-case';
+import { FechaComprobante } from '../../../../src/cfdi33/standard/fecha-comprobante';
+import { DateTime } from 'luxon';
+import { Status } from '../../../../src/status';
+
+describe('FechaComprobante', () => {
+    const { setValidator, getComprobante33, runValidate, assertStatusEqualsCode, getAsserts } = useValidate33TestCase();
+    let validator: FechaComprobante;
+
+    beforeEach(() => {
+        validator = new FechaComprobante();
+        setValidator(validator);
+    });
+
+    test('construct without arguments', () => {
+        const expectedTolerance = 300;
+        const expectedMaxTime = Date.now() + expectedTolerance;
+
+        const validator = new FechaComprobante();
+
+        expect(validator.canValidateCfdiVersion('3.3')).toBeTruthy();
+        expect(validator.getMaximumDate()).toBe(expectedMaxTime + 1);
+        expect(validator.getTolerance()).toBe(expectedTolerance);
+    });
+
+    test('set maximum date', () => {
+        const validator = new FechaComprobante();
+        validator.setMaximumDate(0);
+        expect(validator.getMaximumDate()).toBe(0);
+    });
+
+    test('set tolerance', () => {
+        const validator = new FechaComprobante();
+        validator.setTolerance(1000);
+        expect(validator.getTolerance()).toBe(1000);
+    });
+
+    test('validate ok current date', async () => {
+        const timestamp = Date.now();
+        getComprobante33().addAttributes({
+            Fecha: DateTime.fromMillis(timestamp).toFormat("yyyy-LL-dd'T'HH:mm:ss"),
+        });
+
+        await runValidate();
+
+        assertStatusEqualsCode(Status.ok(), 'FECHA01');
+        assertStatusEqualsCode(Status.ok(), 'FECHA02');
+        expect(getAsserts().hasErrors()).toBeFalsy();
+    });
+
+    test('validate ok minimum date', async () => {
+        const now = validator.getMinimumDate();
+        getComprobante33().addAttributes({
+            Fecha: DateTime.fromMillis(now).toFormat("yyyy-LL-dd'T'HH:mm:ss"),
+        });
+
+        await runValidate();
+
+        assertStatusEqualsCode(Status.ok(), 'FECHA01');
+        assertStatusEqualsCode(Status.ok(), 'FECHA02');
+        expect(getAsserts().hasErrors()).toBeFalsy();
+    });
+
+    test('validate ok maximum date', async () => {
+        const nowMax = validator.getMaximumDate();
+        getComprobante33().addAttributes({
+            Fecha: DateTime.fromMillis(nowMax).toFormat("yyyy-LL-dd'T'HH:mm:ss"),
+        });
+
+        await runValidate();
+
+        assertStatusEqualsCode(Status.ok(), 'FECHA01');
+        assertStatusEqualsCode(Status.ok(), 'FECHA02');
+        expect(getAsserts().hasErrors()).toBeFalsy();
+    });
+
+    test('validate without fecha', async () => {
+        await runValidate();
+
+        assertStatusEqualsCode(Status.error(), 'FECHA01');
+        assertStatusEqualsCode(Status.none(), 'FECHA02');
+    });
+
+    test('validate empty fecha', async () => {
+        getComprobante33().addAttributes({
+            Fecha: '',
+        });
+
+        await runValidate();
+
+        assertStatusEqualsCode(Status.error(), 'FECHA01');
+        assertStatusEqualsCode(Status.none(), 'FECHA02');
+    });
+
+    test('validate malformed fecha', async () => {
+        getComprobante33().addAttributes({
+            Fecha: 'YYYY-MM-DD hh:mm:ss',
+        });
+
+        await runValidate();
+
+        assertStatusEqualsCode(Status.error(), 'FECHA01');
+        assertStatusEqualsCode(Status.none(), 'FECHA02');
+    });
+
+    test('validate older fecha', async () => {
+        getComprobante33().addAttributes({
+            Fecha: '2017-06-30T23:59:59',
+        });
+
+        await runValidate();
+
+        assertStatusEqualsCode(Status.ok(), 'FECHA01');
+        assertStatusEqualsCode(Status.error(), 'FECHA02');
+    });
+
+    test('validate future fecha', async () => {
+        getComprobante33().addAttributes({
+            Fecha: DateTime.fromMillis(validator.getMaximumDate() + 1000).toFormat("yyyy-LL-dd'T'HH:mm:ss"),
+        });
+
+        await runValidate();
+
+        assertStatusEqualsCode(Status.ok(), 'FECHA01');
+        assertStatusEqualsCode(Status.error(), 'FECHA02');
+    });
+});
